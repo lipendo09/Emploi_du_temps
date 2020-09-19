9ad631eeaa49a934932adb4bc1262df4313b745c
Mathurin <mattb2918@gmail.com>
2020-09-19 17:31:05 +0000

    authentification de l'admin et de l'utilisateur

---
+++ resources/views/layouts/partials/_links.blade.php
@@ -0,0 +1,2 @@
+<link rel="stylesheet" href="{{asset('cssETjs/css/bootstrap.min.css')}}">
+<link rel="stylesheet" href="{{asset('cssETjs/css/style.css')}}">
---
+++ resources/views/layouts/partials/_scripts.blade.php
@@ -0,0 +1,3 @@
+<script src="{{asset('cssETjs/js/popper.js')}}"></script>
+<script src="{{asset('cssETjs/js/jquery.slim.min.js')}}"></script>
+<script src="{{asset('cssETjs/js/bootstrap.min.js')}}"></script>
